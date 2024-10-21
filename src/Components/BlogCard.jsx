fdaa5cbae84e76db189c1b6e0b0cbbc0c1cb5e5d
rameem2003 <rameem2019@gmail.com>
2024-10-21 19:56:05 +0600

    update from rammeem

--- src/Components/BlogCard.jsx
+++ src/Components/BlogCard.jsx
@@ -1,9 +1,24 @@
 import React from "react";
 
-const BlogCard = ({ children }) => {
+const BlogCard = () => {
   return (
     <div className="py-8 px-12 bg-[#FFF5EB] max-w-[302px] mb-[126px] mt-[64px]">
-      {children}
+      <h5 className="text-[#DC9853] text-[12px] font-bold">Relationship</h5>
+      <h2 className="text-[24px] font-bold w-[218px] my-4">
+        WATCH AND LISTEN TO OUR SERMONS
+      </h2>
+      <p className=" w-[238px] mb-[48px]">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor.
+      </p>
+      <div>
+        <h4 className="text-[16px] font-normal leading-[24px]">
+          By Mathew Johnson
+        </h4>
+        <h4 className="text-[16px] font-normal leading-[24px]">
+          Tuesday 13 May, 2021
+        </h4>
+      </div>
     </div>
   );
 };
--- src/Pages/Blog.jsx
+++ src/Pages/Blog.jsx
@@ -1,9 +1,9 @@
 import React from "react";
+import Container from "../Components/Container";
 import Header from "./../ComponentsHeader/Header";
 import HeaddingH5 from "../ComponentsHeader/HeaddingH5";
 import HeaddingH2 from "./../ComponentsHeader/HeaddingH2";
 import BlogCard from "../Components/BlogCard";
-import Container from "../Components/Container";
 
 const Blog = () => {
   return (
@@ -13,87 +13,12 @@ const Blog = () => {
           <HeaddingH5 childern="Read our Blog" />
           <HeaddingH2 childern="SHARE, INSPIRE, INNOVATE" />
         </Header>
-        <div className="flex justify-between text-center">
-          <BlogCard>
-            <h5 className="text-[#DC9853] text-[12px] font-bold">
-              Relationship
-            </h5>
-            <h2 className="text-[24px] font-bold w-[218px] my-4">
-              WATCH AND LISTEN TO OUR SERMONS{" "}
-            </h2>
-            <p className=" w-[238px] mb-[48px]">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor.
-            </p>
-            <div>
-              <h4 className="text-[16px] font-normal leading-[24px]">
-                By Mathew Johnson
-              </h4>
-              <h4 className="text-[16px] font-normal leading-[24px]">
-                Tuesday 13 May, 2021
-              </h4>
-            </div>
-          </BlogCard>
-          <BlogCard>
-            <h5 className="text-[#DC9853] text-[12px] font-bold">
-              Relationship
-            </h5>
-            <h2 className="text-[24px] font-bold w-[218px] my-4">
-              WATCH AND LISTEN TO OUR SERMONS{" "}
-            </h2>
-            <p className=" w-[238px] mb-[48px]">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor.
-            </p>
-            <div>
-              <h4 className="text-[16px] font-normal leading-[24px]">
-                By Mathew Johnson
-              </h4>
-              <h4 className="text-[16px] font-normal leading-[24px]">
-                Tuesday 13 May, 2021
-              </h4>
-            </div>
-          </BlogCard>
-          <BlogCard>
-            <h5 className="text-[#DC9853] text-[12px] font-bold">
-              Relationship
-            </h5>
-            <h2 className="text-[24px] font-bold w-[218px] my-4">
-              WATCH AND LISTEN TO OUR SERMONS{" "}
-            </h2>
-            <p className=" w-[238px] mb-[48px]">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor.
-            </p>
-            <div>
-              <h4 className="text-[16px] font-normal leading-[24px]">
-                By Mathew Johnson
-              </h4>
-              <h4 className="text-[16px] font-normal leading-[24px]">
-                Tuesday 13 May, 2021
-              </h4>
-            </div>
-          </BlogCard>
-          <BlogCard>
-            <h5 className="text-[#DC9853] text-[12px] font-bold">
-              Relationship
-            </h5>
-            <h2 className="text-[24px] font-bold w-[218px] my-4">
-              WATCH AND LISTEN TO OUR SERMONS{" "}
-            </h2>
-            <p className=" w-[238px] mb-[48px]">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor.
-            </p>
-            <div>
-              <h4 className="text-[16px] font-normal leading-[24px]">
-                By Mathew Johnson
-              </h4>
-              <h4 className="text-[16px] font-normal leading-[24px]">
-                Tuesday 13 May, 2021
-              </h4>
-            </div>
-          </BlogCard>
+
+        <div className="flex justify-between">
+          <BlogCard />
+          <BlogCard />
+          <BlogCard />
+          <BlogCard />
         </div>
       </Container>
     </div>

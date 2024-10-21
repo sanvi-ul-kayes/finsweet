95ec6427c2880e0864e60a09cb5db9bc0937f91b
rameem2003 <rameem2019@gmail.com>
2024-10-21 20:15:09 +0600

    update from rammeem 2

--- src/Components/BlogCard.jsx
+++ src/Components/BlogCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const BlogCard = () => {
   return (
-    <div className="py-8 px-12 bg-[#FFF5EB] max-w-[302px] mb-[126px] mt-[64px]">
+    <div className="py-8 px-12 bg-[#FFF5EB] max-w-[302px] mb-[126px] mt-[64px] relative group">
       <h5 className="text-[#DC9853] text-[12px] font-bold">Relationship</h5>
       <h2 className="text-[24px] font-bold w-[218px] my-4">
         WATCH AND LISTEN TO OUR SERMONS
@@ -19,6 +19,8 @@ const BlogCard = () => {
           Tuesday 13 May, 2021
         </h4>
       </div>
+
+      <div className=" w-full h-[16px] bg-[#DC9853] absolute left-0 bottom-0 opacity-0 duration-300 ease-in-out group-hover:opacity-100"></div>
     </div>
   );
 };
